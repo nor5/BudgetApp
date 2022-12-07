cfd5fc5a007a2800733884ad39260a2b3032e7a2
nor5 <noorbelkacem5@gmail.com>
2022-12-07 17:22:57 +0100

    first commit

---
+++ budget.py
@@ -0,0 +1,11 @@
+class Category:
+  def __init__(self, name, ledger):
+        # Instance variable
+        self.name = name
+        self.ledger = ledger
+
+
+
+
+
+def create_spend_chart(categories):
