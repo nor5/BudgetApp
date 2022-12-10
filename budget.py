2232c5a78cc1383b188032382d21ded35e40d03d
nor5 <noorbelkacem5@gmail.com>
2022-12-10 17:26:10 +0100

    first commit

--- budget.py
+++ budget.py
@@ -1,81 +1,77 @@
+import math
 class Category:
+
   def __init__(self, name):
-        # Instance variable
-        self.name = name
-        self.ledger = []
-  def deposit (self, amount , description):
-       
-        self.ledger.append({"amount": amount, "description": description})
-  
-  
-  
-  
+    # Instance variable
+    self.name = name
+    self.ledger = []
+    self.categorie_spent = 0
+  def deposit(self, amount, description=''):
 
-  
+    self.ledger.append({"amount": amount, "description": description})
 
-  def withdraw (self, amount , description):
+  def withdraw(self, amount, description=''):
     if self.check_funds(amount):
-       self.ledger.append({"amount": -amount, "description": description})
-       return True
+      self.ledger.append({"amount": -amount, "description": description})
+      self.categorie_spent += amount
+      return True
     else:
       return False
 
-
-  def get_balance (self):
+  def get_balance(self):
     balance = 0
     for a in self.ledger:
       balance = balance + a["amount"]
     return balance
-##    
-  def transfer (self, amount, budget2):
-    discription1 = "Transfer to "+ budget2.name
-    discription2 = "Transfer from "+ self.name
+##
+
+  def transfer(self, amount, budget2):
+    discription1 = "Transfer to " + budget2.name
+    discription2 = "Transfer from " + self.name
     if self.check_funds(amount):
-      self.withdraw(amount , discription1 )
-      budget2.deposit(amount , discription2 )
+      self.withdraw(amount, discription1)
+      self.categorie_spent += amount
+      budget2.deposit(amount, discription2)
       return True
     else:
       return False
-    
-  
-##
-  def check_funds (self, amount):
+
+
+
+
+  def check_funds(self, amount):
     if amount > self.get_balance():
       return False
     else:
       return True
+
   #printing object Category
   def __str__(self):
-    line1 = ""
-   
-    title = ((30 - len(self.name) ) // 2) * "*"+self.name + ((30 - len(self.name) ) // 2) * "*"
-    for i in self.ledger:
-     # amountFormating = "{:.2f}".format(i["amount"])
-      line1 = line1 + i["description"][:23] +" "+ str("{:.2f}".format(i["amount"]))[-7:]+"\n"
-      
-    total = "Total "+str( "{:.2f}".format(self.get_balance()))
-    
+    line = ""
 
-    
-    return line1 +total
-    
-    
-##
-##
-##
-##
-##def create_spend_chart(categories)
-food = Category("foood")
-clothing = Category("clothing")
-food.deposit(1000 ,"initiale deposit")
-clothing.deposit(100 ,"groceries")
-food.withdraw (52.45643,"groceries")
-o = food.withdraw (24,"restaurant and more food")
-food.transfer(123, clothing )
-print(food.get_balance())
-print(o)
-print(food.ledger)
-print(food.name)
-print(clothing.ledger)
-print("object food")
-print (food)
+    title = (self.name).center(30, "*") + "\n"
+    #title = ((30 - len(self.name) ) // 2) * "*"+self.name + ((30 - len(self.name) ) // 2) * "*"+"\n"
+    for i in self.ledger:
+
+      #line += str(i['description'][:23]).ljust(23) + for_num.rjust(7) + '\n'
+
+      line += (i["description"][:23]).ljust(23) + " " + ("{0:.2f}".format(
+        i["amount"])[-7:]).rjust(7) + "\n"
+
+    total = "Total " + str("{:.2f}".format(self.get_balance()))
+
+    return title + line + total
+
+
+def create_spend_chart(categories):
+   total_spent = 0
+   for categorie in categories:
+      total_spent += categorie.categorie_spent
+      print("total spent" +str(total_spent))
+      
+   for categorie in categories:
+      
+      percentage = math.floor(categorie.categorie_spent/total_spent*10)*10
+      print("%"+str(percentage) )
+
+   return "byebye"
